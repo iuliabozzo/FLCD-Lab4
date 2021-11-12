82ac190529b9a830d6690928e3d12ef59c99e775
iuliabozzo <56926554+iuliabozzo@users.noreply.github.com>
2021-11-12 17:12:17 +0200

    final form

--- finite_automaton.py
+++ finite_automaton.py
@@ -1,11 +1,11 @@
 class FiniteAutomaton:
 
-    def __init__(self, Q, sigma, q0, F, S):
-        self._Q = Q
-        self._sigma = sigma
-        self._q0 = q0
-        self._F = F
-        self._S = S
+    def __init__(self, Q, sigma, q0, F, transitions):
+        self.Q = Q
+        self.sigma = sigma
+        self.q0 = q0
+        self.F = F
+        self.transitions = transitions
 
     @staticmethod
     def line_to_list(line):
@@ -28,45 +28,58 @@ class FiniteAutomaton:
             raise Exception("Transition is invalid")
 
     @staticmethod
+    def already_added_transition(crt_state, input, next_state, transitions):
+        for t in transitions[(crt_state, input)]:
+            if t == next_state:
+                return True
+        return False
+
+    @staticmethod
     def read_from_file(file_name):
         with open(file_name) as file:
             Q = FiniteAutomaton.line_to_list(file.readline())
-            E = FiniteAutomaton.line_to_list(file.readline())
+            sigma = FiniteAutomaton.line_to_list(file.readline())
             q0 = FiniteAutomaton.line_to_list(file.readline())[0]
             F = FiniteAutomaton.line_to_list(file.readline())
 
             FiniteAutomaton.validate_q0(q0, Q)
             FiniteAutomaton.validate_F(F, Q)
 
-            S = {}
+            transitions = {}
 
             for t in file:
                 crt_state = t.strip().split('->')[0].strip().replace('(', '').replace(')', '').split(',')[0]
                 input = t.strip().split('->')[0].strip().split(',')[1]
                 next_state = t.strip().split('->')[1].strip()
-                FiniteAutomaton.validate_transition(crt_state, input, next_state, E, Q)
-                if (crt_state, input) in S.keys():
-                    S[(crt_state, input)].add(next_state)
+                FiniteAutomaton.validate_transition(crt_state, input, next_state, sigma, Q)
+                if (crt_state, input) in transitions.keys():
+                    if FiniteAutomaton.already_added_transition(crt_state, input, next_state, transitions) is False:
+                        transitions[(crt_state, input)].append(next_state)
                 else:
-                    S[(crt_state, input)] = set()
-                    S[(crt_state, input)].add(next_state)
+                    transitions[(crt_state, input)] = [next_state]
 
-            return FiniteAutomaton(Q, E, q0, F, S)
+            return FiniteAutomaton(Q, sigma, q0, F, transitions)
 
     def is_deterministic(self):
-        for state in self._S.keys():
-            if len(self._S[state]) != 1:
+        for state in self.transitions.keys():
+            if len(self.transitions[state]) != 1:
                 return False
         return True
 
     def is_accepted(self, input):
         if self.is_deterministic():
-            crt_state = self._q0
+            crt_state = self.q0
             for symbol in input:
                 try:
-                    crt_state = self._S[(crt_state, symbol)][0]
+                    crt_state = self.transitions[(crt_state, symbol)][0]
                 except KeyError:
                     return False
-            return crt_state in self._F
+            return crt_state in self.F
         return False
 
+    def __str__(self):
+        return "states = { " + ', '.join(self.Q) + " }\n" \
+                "alphabet = { " + ', '.join(self.sigma) + " }\n" \
+                "q0 = { " + self.q0 + " }\n" \
+                "final_states = { " + ', '.join(self.F) + " }\n" \
+                "transitions = { " + str(self.transitions) + " }"
--- main.py
+++ main.py
@@ -1,5 +1,64 @@
 from finite_automaton import *
 
-fa = FiniteAutomaton.read_from_file("FA.in")
-print(fa.is_deterministic())
-print(fa.is_accepted("abc1"))
+class Main:
+    def __init__(self):
+        self._fa = FiniteAutomaton.read_from_file('FA.in')
+
+    def print_all(self):
+        print(self._fa)
+
+    def print_states(self):
+        print(self._fa.Q)
+
+    def print_alphabet(self):
+        print(self._fa.sigma)
+
+    def print_initial_state(self):
+        print(self._fa.q0)
+
+    def print_final_states(self):
+        print(self._fa.F)
+
+    def print_transitions(self):
+        print(self._fa.transitions)
+
+    def print_dfa_check(self):
+        print("Yes, it is DFA") if self._fa.is_deterministic() else print("No, it is not DFA")
+
+    def check_sequence(self):
+        seq = input()
+        print(self._fa.is_accepted(seq))
+
+    def run(self):
+        commands = {
+                    '1': self.print_all,
+                    '2': self.print_states,
+                    '3': self.print_alphabet,
+                    '4': self.print_initial_state,
+                    '5': self.print_final_states,
+                    '6': self.print_transitions,
+                    '7': self.print_dfa_check,
+                    '8': self.check_sequence
+                    }
+        ok = False
+        while not ok:
+            print("0. Exit")
+            print("1. Display the finite automata")
+            print("2. Display all states")
+            print("3. Display the alphabet")
+            print("4. Display the initial state/q0")
+            print("5. Display the final states")
+            print("6. Display all transitions")
+            print("7. Check if FA is DFA")
+            print("8. Check sequence")
+            print(">>")
+            cmd = input()
+            if cmd in commands.keys():
+                commands[cmd]()
+            elif cmd == "0":
+                ok = True
+            else:
+                continue
+
+main = Main()
+main.run()
